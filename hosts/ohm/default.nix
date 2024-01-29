4cd09dac9d611c4b78b623d110bfdf85b25ea46e
✿ corey <corey@x64.co>
2024-01-29 12:21:01 -0800

    Setup user

--- hosts/ohm/default.nix
+++ hosts/ohm/default.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }: {
   imports = [
+    ./system.nix
     ./hardware-configuration.nix
     ./networking.nix # generated at runtime by nixos-infect
   ];
@@ -9,5 +10,4 @@
   networking.hostName = "ohm";
   networking.domain = "";
   services.openssh.enable = true;
-  users.users.root.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICqvZ7US1q9NDUo15xyyzXCiAGXoes0tmETy/76+nG7A corey@x64.co'' ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICt1ZqGf+/PV2hiuGVxbJvytEcKT0xvd4+iJZlFKtAQr corey@x64.co'' ];
 }
--- hosts/ohm/system.nix
+++ hosts/ohm/system.nix
@@ -1,3 +1,14 @@
 { config, pkgs, ... }: {
+  users = {
+    mutableUsers = false;
+    users.mu = {
+      isNormalUser = true;
+      extraGroups = [ "wheel"];
+      shell = pkgs.zsh;
+    };
+  };
+  users.users.root.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICqvZ7US1q9NDUo15xyyzXCiAGXoes0tmETy/76+nG7A corey@x64.co'' ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICt1ZqGf+/PV2hiuGVxbJvytEcKT0xvd4+iJZlFKtAQr corey@x64.co'' ];
+  users.users.mu.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICqvZ7US1q9NDUo15xyyzXCiAGXoes0tmETy/76+nG7A corey@x64.co'' ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICt1ZqGf+/PV2hiuGVxbJvytEcKT0xvd4+iJZlFKtAQr corey@x64.co'' ];
+
   system.stateVersion = "24.05";
 }
