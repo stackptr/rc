1b5a5e504bdf8f46d9b755d25698ca3a83b34eee
✿ corey (they/them) <corey@x64.co>
2024-02-06 19:33:48 -0800

    Consolidate `darwinConfigurations`

--- flake.nix
+++ flake.nix
@@ -34,15 +34,27 @@
           }
         ];
       };
-    mkHosts = hosts:
+    darwinHost = {hostname, ...}:
+      nix-darwin.lib.darwinSystem {
+        modules = [
+          ./hosts/${hostname}
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.corey = import ./home;
+          }
+        ];
+      };
+    mkHosts = f: hosts:
       builtins.listToAttrs (map
         (host: {
           name = host.hostname;
-          value = nixosHost host;
+          value = f host;
         })
         hosts);
   in {
-    nixosConfigurations = mkHosts [
+    nixosConfigurations = mkHosts nixosHost [
       {
         hostname = "zeta";
         system = "aarch64-linux";
@@ -53,28 +65,13 @@
       }
     ];
 
-    darwinConfigurations."Rhizome" = nix-darwin.lib.darwinSystem {
-      modules = [
-        ./hosts/Rhizome
-        home-manager.darwinModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.corey = import ./home;
-        }
-      ];
-    };
-
-    darwinConfigurations."Petrichor" = nix-darwin.lib.darwinSystem {
-      modules = [
-        ./hosts/Petrichor
-        home-manager.darwinModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.corey = import ./home;
-        }
-      ];
-    };
+    darwinConfigurations = mkHosts darwinHost [
+      {
+        hostname = "Rhizome";
+      }
+      {
+        hostname = "Petrichor";
+      }
+    ];
   };
 }
