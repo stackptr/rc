acef5ca70dd240c693cb24e244a351f1dd7949ef
✿ corey (they/them) <corey@x64.co>
2024-02-14 22:16:44 -0800

    Formatting

--- secrets.nix
+++ secrets.nix
@@ -1,8 +1,8 @@
 let
-  firstLine = str: builtins.head (builtins.split "\n" str); 
+  firstLine = str: builtins.head (builtins.split "\n" str);
   readKey = host:
     firstLine (builtins.readFile ./hosts/${host}/key.pub);
-  keys = map readKey [ "ohm" "Petrichor" "Rhizome" "zeta"];
+  keys = map readKey ["ohm" "Petrichor" "Rhizome" "zeta"];
 in {
   "hosts/zeta/secrets/userpassword.age".publicKeys = keys;
   "hosts/zeta/secrets/wireless.age".publicKeys = keys;
