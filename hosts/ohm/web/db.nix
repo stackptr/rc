e7e66c0b370e6e55d5d01589b0214a11aba21c71
✿ corey (they/them) <corey@x64.co>
2024-02-29 15:58:28 -0800

    Bump PG max_connections

--- hosts/ohm/web/db.nix
+++ hosts/ohm/web/db.nix
@@ -26,6 +26,9 @@
       host   all   all   127.0.0.1/32   trust
       host   all   all   100.64.0.0/10  trust
     '';
+    settings = {
+      max_connections = 200;
+    };
   };
   services.postgresqlBackup = {
     enable = true;
