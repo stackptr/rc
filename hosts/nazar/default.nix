36c646cc1e386b32cdb377c7279ebdced1cbd959
✿ corey (they/them) <corey@x64.co>
2024-03-07 21:50:40 -0800

    Remove comments

--- hosts/nazar/default.nix
+++ hosts/nazar/default.nix
@@ -9,11 +9,9 @@
     ./disk-config.nix
   ];
 
-  # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   networking.hostName = "nazar";
   networking.networkmanager.enable = true;
@@ -22,5 +20,5 @@
   i18n.defaultLocale = "en_US.UTF-8";
   services.openssh.enable = true;
 
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.05";
 }
