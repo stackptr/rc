9ed755ae5cf8947eca03c288c435edc94195cbdb
✿ corey <corey@x64.co>
2024-03-20 17:16:31 -0700

    Set Darwin system defaults

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -8,6 +8,90 @@
 
   system.configurationRevision = self.rev or self.dirtyRev or null;
 
+  system.defaults = {
+    ".GlobalPreferences"."com.apple.mouse.scaling" = 1.5;
+    NSGlobalDomain = {
+      AppleScrollerPagingBehavior = true; # Jump to spot on scroll bar when clicked
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticDashSubstitutionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false; # Disable smart quoting
+      NSAutomaticSpellingCorrectionEnabled = false;
+      "com.apple.springing.enabled" = true;
+      "com.apple.springing.delay" = 0.5;
+      # "com.apple.trackpad.forceClick" = 1; # TODO
+      "com.apple.trackpad.scaling" = 1.0;
+
+      # Always use expanded save panel
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSNavPanelExpandedStateForSaveMode2 = true;
+
+      # Quickly repeat keys when held
+      InitialKeyRepeat = 15;
+      KeyRepeat = 2;
+    };
+    # TODO:
+    # showAppExposeGestureEnabled = 1;
+    # showMissionControlGestureEnabled = 1;
+    dock = {
+      appswitcher-all-displays = false;
+      autohide = false;
+      mineffect = "scale";
+      minimize-to-application = false;
+      mru-spaces = false;
+      orientation = "bottom";
+      persistent-apps = [
+        "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app"
+        "/System/Applications/Mail.app"
+        "/Applications/Nova.app"
+        "/Applications/Prompt.app"
+        "/Applications/Reeder.app"
+        "/Applications/Omnivore.app"
+        "/System/Applications/Music.app"
+        "/System/Applications/Calendar.app"
+        "/Applications/Things3.app"
+        "/System/Applications/Notes.app"
+        "/System/Applications/Messages.app"
+        "/Applications/Slack.app"
+        "/Applications/GitHub Desktop.app"
+      ];
+      show-process-indicators = false;
+      showhidden = false;
+      show-recents = false;
+      static-only = false;
+
+      # N.B.: Values from Petrichor; update conditionally for Rhizome
+      tilesize = 49;
+      magnification = true;
+      largesize = 92;
+
+      # Disable hot corners
+      wvous-tl-corner = 1;
+      wvous-bl-corner = 1;
+      wvous-tr-corner = 1;
+      wvous-br-corner = 1;
+    };
+    finder = {
+      AppleShowAllFiles = false;
+      ShowStatusBar = false;
+      ShowPathbar = false;
+      FXPreferredViewStyle = "Nlsv"; # Prefer list view
+      AppleShowAllExtensions = true;
+      FXEnableExtensionChangeWarning = false; # Do not warn when changing file extensions
+    };
+    menuExtraClock = {
+      ShowAMPM = true;
+      ShowDayOfWeek = false;
+      ShowDate = 0; # Show full date
+    };
+    screencapture.location = "~/Downloads";
+    trackpad = {
+      Clicking = true; # tap to click
+      Dragging = true; # tap to drag
+      TrackpadThreeFingerDrag = true;
+    };
+  };
+
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
