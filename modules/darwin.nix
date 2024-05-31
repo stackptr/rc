7f69c31a255a88b521289e7ab326b72ac203631a
✿ corey (they/them) <corey@x64.co>
2024-05-31 10:16:47 -0700

    Pass taps to nix-homebrew

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -12,6 +12,7 @@
     enable = true;
     caskArgs.no_quarantine = true;
     onActivation.cleanup = "zap";
+    taps = builtins.attrNames config.nix-homebrew.taps; # See: zhaofengli/nix-homebrew#5
     # N.B.: prefer casks to nixpkgs for placement in ~/Applications
     casks = [
       "apparency"
