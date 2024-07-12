5ece4538d3af3d520490baf2c8ab2b3f8340539f
✿ corey (they/them) <corey@x64.co>
2024-07-12 10:04:32 -0700

    Fix locale settings

--- hosts/glyph/default.nix
+++ hosts/glyph/default.nix
@@ -29,6 +29,8 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
+    LANGUAGE = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
     LC_MEASUREMENT = "en_US.UTF-8";
