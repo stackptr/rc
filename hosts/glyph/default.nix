03cd4fbfccb27f485d10f4a903ecf24aa2fb2415
✿ corey (they/them) <corey@x64.co>
2024-07-12 15:14:34 -0700

    Enable cockpit

--- hosts/glyph/default.nix
+++ hosts/glyph/default.nix
@@ -28,6 +28,7 @@
   time.timeZone = "America/Los_Angeles";
   i18n.defaultLocale = "en_US.UTF-8";
 
+  services.cockpit.enable = true;
   services.openssh.enable = true;
   services.samba = {
     enable = true;
