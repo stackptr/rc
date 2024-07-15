802f4ee455897407d572687c9ea7821c25a6223b
✿ corey (they/them) <corey@x64.co>
2024-07-15 11:56:37 -0700

    Services

--- hosts/glyph/default.nix
+++ hosts/glyph/default.nix
@@ -6,6 +6,7 @@
 }: {
   imports = [
     ./hardware-configuration.nix
+    ./services
   ];
 
   boot.supportedFilesystems = ["ext4" "zfs"];
@@ -27,31 +28,6 @@
 
   time.timeZone = "America/Los_Angeles";
   i18n.defaultLocale = "en_US.UTF-8";
-
-  services.cockpit.enable = true;
-  services.openssh.enable = true;
-  services.samba = {
-    enable = true;
-    securityType = "user";
-    openFirewall = false; # Only allow over Tailscale
-    invalidUsers = [];
-    shares = {
-      Media = {
-        path = "/mnt/media";
-        browseable = "yes";
-        "read only" = "no";
-        "guest ok" = "no";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-        "force user" = "mu";
-        "force group" = "users";
-      };
-    };
-  };
-  services.tailscale = {
-    enable = true;
-    extraUpFlags = ["--ssh"];
-  };
 
   system.stateVersion = "24.05";
 }
