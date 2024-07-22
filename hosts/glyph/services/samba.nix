7984b956698fa465462c8f07379dae2fd78665dd
✿ corey (they/them) <corey@x64.co>
2024-07-22 15:19:13 -0700

    Improve samba on macOS
    See: https://wiki.samba.org/index.php/Configure_Samba_to_Work_Better_with_Mac_OS_X

--- hosts/glyph/services/samba.nix
+++ hosts/glyph/services/samba.nix
@@ -23,6 +23,16 @@
     securityType = "user";
     openFirewall = false; # Only allow over Tailscale
     invalidUsers = [];
+    extraConfig = ''
+      vfs objects = fruit streams_xattr
+      fruit:metadata = stream
+      fruit:model = MacSamba
+      fruit:posix_rename = yes
+      fruit:veto_appledouble = no
+      fruit:nfs_aces = no
+      fruit:wipe_intentionally_left_blank_rfork = yes
+      fruit:delete_empty_adfiles = yes
+    '';
     shares = {
       Backup = mkShare "/mnt/backup" "mu" "users";
       Media = mkShare "/mnt/media" config.services.plex.user config.services.plex.group;
