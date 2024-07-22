993dc505c32f51f5c8826d0622c615623d775fe5
✿ corey (they/them) <corey@x64.co>
2024-07-22 09:34:32 -0700

    Open firewall for Plex

--- hosts/glyph/services/default.nix
+++ hosts/glyph/services/default.nix
@@ -10,7 +10,10 @@
 
   services.cockpit.enable = true;
   services.openssh.enable = true;
-  services.plex.enable = true;
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+  };
   services.samba = {
     enable = true;
     securityType = "user";
