763057fbc7bf5b4239f6f82e558af376c564bd41
✿ corey (they/them) <corey@x64.co>
2024-08-21 15:29:01 -0700

    Flatten inputs

--- flake.nix
+++ flake.nix
@@ -3,11 +3,15 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     agenix.url = "github:ryantm/agenix";
+    agenix.inputs.darwin.follows = "nix-darwin";
+    agenix.inputs.home-manager.follows = "home-manager";
+    agenix.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+    nix-homebrew.inputs.nix-darwin.follows = "nix-darwin";
     homebrew-bundle = {
       url = "github:homebrew/homebrew-bundle";
       flake = false;
@@ -21,6 +25,7 @@
       flake = false;
     };
     profile.url = "github:stackptr/profile";
+    profile.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs:
