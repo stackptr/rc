24fed1a99b2901c6f772373956d1531ac4e18e43
✿ corey (they/them) <corey@x64.co>
2024-09-13 16:10:39 -0700

    Allow insecure olm

--- hosts/nazar/web/matrix/default.nix
+++ hosts/nazar/web/matrix/default.nix
@@ -9,4 +9,8 @@
     ./heisenbridge.nix
     ./discord.nix
   ];
+  
+  nixpkgs.config.permittedInsecurePackages = [
+    "olm-3.2.16" # TODO: https://github.com/NixOS/nixpkgs/pull/334638#issuecomment-2289025802
+  ];
 }
