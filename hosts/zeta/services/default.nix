3166d6b6c3c5be787919c89ec6094bb4cb71680c
✿ corey (they/them) <corey@x64.co>
2025-01-29 14:33:44 -0800

    Remove services

--- hosts/zeta/services/default.nix
+++ hosts/zeta/services/default.nix
@@ -3,51 +3,11 @@
   pkgs,
   ...
 }: {
-  imports = [
-    ./home-assistant.nix
-    ./homebridge.nix
-    ./znc.nix
-  ];
-  age.secrets.dd-agent = {
-    file = ./../secrets/dd-agent.age;
-    mode = "440";
-    owner = "datadog";
-    group = "datadog";
-  };
   services.openssh.enable = true;
   services.tailscale = {
     enable = true;
     extraUpFlags = ["--ssh"];
   };
-  services.samba = {
-    enable = true;
-    securityType = "user";
-    openFirewall = true;
-    invalidUsers = [];
-    shares = {
-      zeta = {
-        path = "/";
-        browseable = "yes";
-        "read only" = "no";
-        "guest ok" = "no";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-        "force user" = "root";
-        "force group" = "root";
-      };
-    };
-  };
-  services.datadog-agent = {
-    enable = true;
-    hostname = "zeta";
-    apiKeyFile = config.age.secrets.dd-agent.path;
-    site = "us3.datadoghq.com";
-    enableLiveProcessCollection = true;
-  };
-  services.avahi = {
-    enable = true;
-  };
-
   programs.mosh.enable = true;
   programs.git.enable = true;
   programs.gnupg.agent.enable = true;
