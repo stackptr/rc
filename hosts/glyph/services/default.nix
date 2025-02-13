f62477534c42cdb60a79ee0573cdce659347fc24
✿ corey (they/them) <corey@x64.co>
2025-02-13 15:26:24 -0800

    Open all ports in local net

--- hosts/glyph/services/default.nix
+++ hosts/glyph/services/default.nix
@@ -45,10 +45,10 @@
     ];
     # Dynamically allocated ports for Roon Bridge opened for local network
     extraCommands = ''
-      iptables -A nixos-fw -p tcp --dport 32768:65535 -j nixos-fw-accept
-      iptables -A nixos-fw -p udp --dport 32768:65535 -j nixos-fw-accept
-      iptables -A nixos-fw -p tcp --dport 32768:65535 -j nixos-fw-accept
-      iptables -A nixos-fw -p udp --dport 32768:65535 -j nixos-fw-accept
+      iptables -A nixos-fw -p tcp -s 192.168.0.0/24 -j nixos-fw-accept
+      iptables -A nixos-fw -p udp -s 192.168.0.0/24 -j nixos-fw-accept
+      iptables -A nixos-fw -p tcp -s 127.0.0.0/8 -j nixos-fw-accept
+      iptables -A nixos-fw -p udp -s 127.0.0.0/8 -j nixos-fw-accept
     '';
   };
   services.tailscale = {
