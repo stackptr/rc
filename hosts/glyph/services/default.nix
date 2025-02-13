f16155c7a2035ab122d34792ef9786f32910dc5b
✿ corey (they/them) <corey@x64.co>
2025-02-13 15:41:10 -0800

    Update subnet

--- hosts/glyph/services/default.nix
+++ hosts/glyph/services/default.nix
@@ -45,8 +45,8 @@
     ];
     # Dynamically allocated ports for Roon Bridge opened for local network
     extraCommands = ''
-      iptables -A nixos-fw -p tcp -j --dport 30000:65535 -s 192.168.0.0/16 nixos-fw-accept
-      iptables -A nixos-fw -p udp -j --dport 30000:65535 -s 192.168.0.0/16 nixos-fw-accept
+      iptables -A nixos-fw -p tcp -j --dport 30000:65535 -s 192.168.4.0/24 nixos-fw-accept
+      iptables -A nixos-fw -p udp -j --dport 30000:65535 -s 192.168.4.0/24 nixos-fw-accept
     '';
   };
   services.tailscale = {
