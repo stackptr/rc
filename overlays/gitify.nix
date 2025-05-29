aec72cdd8bff796508cd91d436213c267bbb6b56
✿ corey (they/them) <c@rey.foo>
2025-05-28 17:12:11 -0700

    Add TODO

--- overlays/gitify.nix
+++ overlays/gitify.nix
@@ -1,3 +1,5 @@
+# TODO: Building from source breaks OAuth flow; use release artifact instead.
+# See: https://github.com/gitify-app/gitify/issues/2013#issuecomment-2917834558
 self: super: let
   version = "6.4.1";
   src = super.fetchFromGitHub {
