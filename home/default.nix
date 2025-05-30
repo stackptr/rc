89e2fb43c4230d5dcbe6c25de0bf3fcca6d5f786
✿ corey (they/them) <c@rey.foo>
2025-05-30 14:46:42 -0700

    direnv config

--- home/default.nix
+++ home/default.nix
@@ -159,6 +159,12 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    config = {
+      global = {
+        strict_env = true;
+        hide_env_diff = true;
+      };
+    };
   };
 
   programs.gh = {
