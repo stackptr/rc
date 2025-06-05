d7e792bb62918e27d7ea1c818b08a33ee67ad71e
✿ corey (they/them) <c@rey.foo>
2025-06-05 14:39:31 -0700

    Set catppuccin nvim `color_overrides`

--- home/default.nix
+++ home/default.nix
@@ -157,7 +157,14 @@
     plugins = [pkgs.vimPlugins.catppuccin-nvim];
     extraLuaConfig = ''
       require("catppuccin").setup {
-        flavour = "mocha"
+        flavour = "mocha",
+        color_overrides = {
+            mocha = {
+                base = "#000000",
+                mantle = "#000000",
+                crust = "#000000",
+            },
+        },
       }
       vim.cmd.colorscheme "catppuccin"
     '';
