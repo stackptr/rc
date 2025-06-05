5a3532add2cf2c8330e7708977bfcaf08de9bbcb
✿ corey (they/them) <c@rey.foo>
2025-06-05 12:07:29 -0700

    tmux configuration

--- home/default.nix
+++ home/default.nix
@@ -392,7 +392,21 @@
           set -g @continuum-save-interval '60' # minutes
         '';
       }
-      tmuxPlugins.catppuccin
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavor "mocha"
+          set -g @catppuccin_window_status_style "rounded"
+          set -g status-right-length 100
+          set -g status-left-length 100
+          set -g status-left ""
+          set -g status-right "#{E:@catppuccin_status_application}"
+          set -agF status-right "#{E:@catppuccin_status_cpu}"
+          set -ag status-right "#{E:@catppuccin_status_session}"
+          set -ag status-right "#{E:@catppuccin_status_uptime}"
+        '';
+      }
+      tmuxPlugins.cpu
       tmuxPlugins.yank
     ];
     extraConfig = ''
