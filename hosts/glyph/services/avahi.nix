321aeba789e0d87c92930367cc9a37fa43be9363
✿ corey (they/them) <c@rey.foo>
2025-07-08 23:44:00 -0700

    Broadcast smb

--- hosts/glyph/services/avahi.nix
+++ hosts/glyph/services/avahi.nix
@@ -5,7 +5,24 @@
 }: {
   services.avahi = {
     enable = true;
+    nssmdns = true;
+    publish = {
+      enable = true;
+      userServices = true;
+      workstation = true;
+    };
     extraServiceFiles = {
+      smb = ''
+        <?xml version="1.0" standalone='no'?>
+        <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+        <service-group>
+          <name replace-wildcards="yes">%h</name>
+          <service>
+            <type>_smb._tcp</type>
+            <port>445</port>
+          </service>
+        </service-group>
+      '';
       deviceinfo = ''
         <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
         <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
