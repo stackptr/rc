01996857117b1415abbdcaeeef72f11c2ef4e20d
✿ corey (they/them) <c@rey.foo>
2025-07-15 10:45:32 -0700

    Update work dock

--- hosts/Petrichor/dock.nix
+++ hosts/Petrichor/dock.nix
@@ -8,17 +8,17 @@
     largesize = 92;
     persistent-apps = [
       "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app"
-      "/System/Applications/Mail.app"
+      "/Applications/Microsoft Outlook.app"
       "/Applications/Nova.app"
       "/Applications/Prompt.app"
       "/Applications/Reeder.app"
       "/Applications/Roon.app"
       "/System/Applications/Calendar.app"
       "/Applications/Things3.app"
-      "/System/Applications/Notes.app"
       "/Applications/Craft.app"
       "/System/Applications/Messages.app"
       "${pkgs.slack}/Applications/Slack.app"
+      "/Applications/Microsoft Teams.app"
       "${pkgs.github-desktop}/Applications/GitHub Desktop.app"
     ];
   };
