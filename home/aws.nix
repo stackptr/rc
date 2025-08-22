baf7830ca912aaa960f75fe433df64561729f2d1
✿ corey <c@rey.foo>
2025-08-22 13:45:25 -0700

    Update AWS SSO config

--- home/aws.nix
+++ home/aws.nix
@@ -11,14 +11,14 @@
         sso_start_url = "https://d-90675613ab.awsapps.com/start";
         sso_region = "us-east-1";
         sso_account_id = "539282909833";
-        sso_role_name = "Freckle-Dev-Engineers";
+        sso_role_name = "AWSRenFreckleEngineer";
         region = "us-east-1";
       };
       "profile freckle-prod" = {
         sso_start_url = "https://d-90675613ab.awsapps.com/start";
         sso_region = "us-east-1";
         sso_account_id = "853032795538";
-        sso_role_name = "FrecklePlatform";
+        sso_role_name = "AWSRenFreckleEngineer";
         region = "us-east-1";
       };
     };
