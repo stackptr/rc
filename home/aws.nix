8330c0a20e4a6a5f7e326c146ea4bcb1cd0329c9
✿ corey <c@rey.foo>
2025-10-02 17:08:36 -0700

    Update AWS using `sso_session`

--- home/aws.nix
+++ home/aws.nix
@@ -8,18 +8,21 @@
     package = pkgs-stable.awscli2;
     settings = {
       "profile freckle-dev" = {
-        sso_start_url = "https://d-90675613ab.awsapps.com/start";
-        sso_region = "us-east-1";
         sso_account_id = "539282909833";
-        sso_role_name = "AWSRenFreckleEngineer";
+        sso_role_name = "AWSRENFreckleEngineer";
         region = "us-east-1";
+        sso_session = "freckle-dev-engineers";
       };
       "profile freckle-prod" = {
+        sso_account_id = "853032795538";
+        sso_role_name = "AWSRENFreckleEngineer";
+        region = "us-east-1";
+        sso_session = "freckle-dev-engineers";
+      };
+      "sso-session freckle-dev-engineers" = {
         sso_start_url = "https://d-90675613ab.awsapps.com/start";
         sso_region = "us-east-1";
-        sso_account_id = "853032795538";
-        sso_role_name = "AWSRenFreckleEngineer";
-        region = "us-east-1";
+        sso_registration_scopes = "sso:account:access";
       };
     };
   };
