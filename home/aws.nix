93653850c10fee80bd2a1e1f111fe7b01f110a37
✿ corey <c@rey.foo>
2025-10-02 17:20:22 -0700

    AWS config for AI tooling

--- home/aws.nix
+++ home/aws.nix
@@ -24,6 +24,18 @@
         sso_region = "us-east-1";
         sso_registration_scopes = "sso:account:access";
       };
+      "profile ai-tools-prod" = {
+        sso_session = "eng-ai-tools-prod-01";
+        sso_account_id = "705754902799";
+        sso_role_name = "AWSIEAIToolEngineer";
+        region = "us-east-1";
+        output = "json";
+      };
+      "sso-session eng-ai-tools-prod-01" = {
+        sso_start_url = "https://d-9267084214.awsapps.com/start";
+        sso_region = "us-west-2";
+        sso_registration_scopes = "sso:account:access";
+      };
     };
   };
 }
