2da97b3f88a40c59e1611c5f5444b29d3ac6ae15
✿ corey (they/them) <c@rey.foo>
2025-10-09 12:04:54 -0700

    Resolve ssh trace warnings

--- home/default.nix
+++ home/default.nix
@@ -182,8 +182,26 @@
 
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "yes";
+    enableDefaultConfig = false;
     matchBlocks = {
+      "*" = let
+        defaultConfig = {
+          forwardAgent = false;
+          addKeysToAgent = "no";
+          compression = false;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+        };
+      in
+        defaultConfig
+        // {
+          addKeysToAgent = "yes";
+        };
       "github.com" = {
         identityFile = "~/.ssh/id_ed25519";
       };
