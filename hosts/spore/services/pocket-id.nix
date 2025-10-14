390a556c488fbd50607661aaa8e246a1208087cd
✿ corey (they/them) <c@rey.foo>
2025-10-14 15:25:18 -0700

    Use oauth2-proxy in test page

--- hosts/spore/services/pocket-id.nix
+++ hosts/spore/services/pocket-id.nix
@@ -90,7 +90,24 @@ in {
     virtualHosts."test.zx.dev" = {
       forceSSL = true;
       useACMEHost = "zx.dev";
+      locations."=/oauth2/auth" = {
+        proxyPass = "http://127.0.0.1:4180";
+        extraConfig = ''
+          proxy_set_header X-Original-URI $request_uri;
+          proxy_set_header X-Real-IP      $remote_addr;
+          proxy_set_header X-Scheme       $scheme;
+          proxy_set_header X-Forwarded-Host $host;
+        '';
+      };
       locations."= /".extraConfig = ''
+        auth_request /oauth2/auth;
+        error_page 401 = https://${authHost}/oauth2/start?rd=$scheme://$http_host$request_uri;
+
+        auth_request_set $auth_user  $upstream_http_x_auth_request_user;
+        auth_request_set $auth_email $upstream_http_x_auth_request_email;
+        add_header X-Auth-User  $auth_user always;
+        add_header X-Auth-Email $auth_email always;
+
         default_type text/html;
         return 200 '<!doctype html><meta charset="utf-8"><title>Test</title><h1>Test page</h1>';
       '';
