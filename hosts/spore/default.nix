fded0a2aab9adb6eeb3feae5075b76da3618a3ab
✿ corey <corey@x64.co>
2025-10-13 23:19:28 -0700

    Add tailscale to trustedInterfaces

--- hosts/spore/default.nix
+++ hosts/spore/default.nix
@@ -37,6 +37,7 @@
   };
 
   networking.firewall.allowedTCPPorts = [80 443];
+  networking.firewall.trustedInterfaces = ["tailscale0"];
 
   time.timeZone = "America/Los_Angeles";
   i18n.defaultLocale = "en_US.UTF-8";
