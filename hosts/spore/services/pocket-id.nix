80efe57b0b345463b4dbe53b0d8d9a0085917395
✿ corey (they/them) <c@rey.foo>
2025-10-15 14:40:53 -0700

    Remove test vhost

--- hosts/spore/services/pocket-id.nix
+++ hosts/spore/services/pocket-id.nix
@@ -31,44 +31,4 @@
       keyFile = config.age.secrets.oauth2-proxy-env.path;
     };
   };
-
-  services.nginx = {
-    virtualHosts."test.zx.dev" = {
-      forceSSL = true;
-      useACMEHost = "zx.dev";
-      requireAuth = true;
-      locations."/" = {
-        root = "/var/www/test";
-        index = "index.html";
-      };
-    };
-  };
-
-  systemd.tmpfiles.rules = [
-    "d /var/www 0755 root root -"
-    "d /var/www/test 0755 root root -"
-  ];
-  systemd.mounts = let
-    testSite = pkgs.writeTextDir "index.html" ''
-      <!doctype html>
-      <meta charset="utf-8">
-      <title>Protected</title>
-      <body style="font-family:system-ui;margin:2rem">
-        <h1>✅ Authenticated via Pocket ID</h1>
-      </body>
-    '';
-  in [
-    {
-      what = "${testSite}";
-      where = "/var/www/test";
-      type = "none";
-      options = "bind,ro";
-      wantedBy = ["multi-user.target"];
-    }
-  ];
-  systemd.services.nginx = {
-    after = ["var-www-test.mount"];
-    requires = ["var-www-test.mount"];
-    serviceConfig.RequiresMountsFor = ["/var/www/test"];
-  };
 }
