71234320e797cd1b2eed38c526063fd265dba42a
✿ corey (they/them) <c@rey.foo>
2025-10-15 09:28:38 -0700

    Try internal named location

--- hosts/spore/services/pocket-id.nix
+++ hosts/spore/services/pocket-id.nix
@@ -122,16 +122,25 @@ in {
           return 302 https://${authHost}/oauth2/start?rd=$scheme://$http_host$request_uri;
         '';
       };
-      locations."/" = {
-        proxyPass = "http://127.0.0.1";
-        extraConfig = ''
-          auth_request /oauth2/auth;
-          error_page 401 = @oauth2_redirect;
+      locations."= /__ok".extraConfig = ''
+        internal;
+        default_type text/html;
+        return 200 '<!doctype html><meta charset="utf-8"><title>Protected</title>
+        <body style="font-family:system-ui;margin:2rem">
+          <h1>✅ Authenticated via Pocket ID</h1>
+          <p>Hello <strong>$upstream_http_x_auth_request_email</strong> from test.zx.dev.</p>
+          <p><a href="https://${authHost}/oauth2/sign_out">Sign out</a></p>
+        </body>';
+      '';
+      locations."/".extraConfig = ''
+        auth_request /oauth2/auth;
+        error_page 401 = @oauth2_redirect;
 
-          auth_request_set $auth_email $upstream_http_x_auth_request_email;
-          add_header X-Auth-Email $auth_email always;
-        '';
-      };
+        auth_request_set $auth_email $upstream_http_x_auth_request_email;
+        add_header X-Auth-Email $auth_email always;
+
+        try_files /__ok =404;
+      '';
     };
   };
 }
