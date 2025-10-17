62476997d39e12c15dbed4e0fe4146f6531a4161
✿ corey (they/them) <c@rey.foo>
2025-10-17 08:31:33 -0700

    Remove insecure OpenSSL

--- hosts/zeta/services/home-assistant.nix
+++ hosts/zeta/services/home-assistant.nix
@@ -21,12 +21,4 @@
     };
   };
   networking.firewall.allowedTCPPorts = [8123];
-
-  # Home-Assistant currently uses an EOL'd version of OpenSSL
-  # See:
-  # - https://github.com/project-chip/connectedhomeip/issues/25688
-  # - https://github.com/NixOS/nixpkgs/issues/269713
-  nixpkgs.config.permittedInsecurePackages = [
-    "openssl-1.1.1w"
-  ];
 }
