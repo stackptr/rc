e6a4a79ca1724df9d9f8951fccc77404697bee44
✿ corey (they/them) <c@rey.foo>
2025-10-17 15:01:10 -0700

    Support custom keys in `system.disableUpdates`

--- modules/darwin/disable-updates.nix
+++ modules/darwin/disable-updates.nix
@@ -5,33 +5,71 @@
 }:
 with lib; let
   cfg = config.system.disableUpdates;
+
+  updateOptions = {config, ...}: {
+    options = {
+      domain = mkOption {
+        type = types.str;
+        description = "Domain of application for which updates will be disabled";
+      };
+
+      keys = mkOption {
+        type = types.listOf types.str;
+        description = "Keys used for software update";
+      };
+    };
+  };
+
+  # Defaults from: https://github.com/sparkle-project/Sparkle/blob/2.x/Sparkle/SUConstants.m
+  defaultKeys = ["SUEnableAutomaticChecks" "SUAutomaticallyUpdate"];
 in {
   options.system.disableUpdates = mkOption {
-    type = types.listOf types.str;
+    type = with types;
+      listOf (coercedTo str (domain: {
+        inherit domain;
+        keys = defaultKeys;
+      }) (submodule updateOptions));
     default = [];
     example = literalExpression ''
       [
-        "com.panic.Nova"
+        "com.example.App"
+        {
+          domain = "com.enterprise.App";
+          keys = ["CustomSoftwareUpdateFramework"]
+        }
       ]
     '';
-    description = ''
-      List of domains to write defaults that attempt to disable automatic software updates and associated prompts.
+    description = let
+      quoteStr = k: "\"${k}\"";
+      defaultKeysStr = builtins.concatStringsSep " " (map quoteStr defaultKeys);
+    in ''
+      List of domains and associated keys to write defaults that attempt to disable
+      automatic software updates and associated prompts.
 
-      Keys are set according to [constants](https://github.com/sparkle-project/Sparkle/blob/2.x/Sparkle/SUConstants.m)
-      in the Sparkle project, a popular software update framework, as well as some that have been determined
-      experimentally.
+      Domains defined as strings, e.g. "com.example.App", are shorthand for default keys:
+
+      `{ domain = "com.example.App"; keys = [${defaultKeysStr}]; }`
+
+      Default keys are from the Sparkle project, a popular software update framework.
     '';
   };
 
   config = mkIf (length cfg > 0) {
-    system.defaults.CustomUserPreferences = builtins.listToAttrs (map
-      (domain: {
-        name = domain;
-        value = {
-          SUEnableAutomaticChecks = false;
-          SUAutomaticallyUpdate = false;
-        };
-      })
-      cfg);
+    system.defaults.CustomUserPreferences = let
+      mkDisabledValues = keys:
+        builtins.listToAttrs (map (key: {
+            name = key;
+            value = false;
+          })
+          keys);
+    in
+      builtins.listToAttrs (map
+        (
+          e: {
+            name = e.domain;
+            value = mkDisabledValues e.keys;
+          }
+        )
+        cfg);
   };
 }
