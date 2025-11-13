870d73684b9d80ba34122a84427c322e6c6d74d5
✿ corey (they/them) <c@rey.foo>
2025-11-12 17:25:51 -0800

    Inline settings

--- hosts/glyph/services/filebrowser.nix
+++ hosts/glyph/services/filebrowser.nix
@@ -5,28 +5,26 @@
   ...
 }: let
   cfg = config.services.filebrowser-quantum;
-  port = 8080;
-  settings = {
-    server = {
-      inherit port;
-      sources = [
-        {
-          path = "/mnt/media";
-          config = {
-            defaultEnabled = true;
-          };
-        }
-      ];
-    };
-    auth = {
-      adminUsername = "admin";
-    };
-  };
 in {
   services.filebrowser-quantum = {
     enable = true;
     openFirewall = false;
-    inherit settings;
+    settings = {
+      server = {
+        port = 8080;
+        sources = [
+          {
+            path = "/mnt/media";
+            config = {
+              defaultEnabled = true;
+            };
+          }
+        ];
+      };
+      auth = {
+        adminUsername = "admin";
+      };
+    };
   };
   users.users.${cfg.user}.extraGroups = ["media"];
 }
