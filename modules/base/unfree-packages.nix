8f73819b9367d895df95f04bdf673933cb017399
✿ corey (they/them) <c@rey.foo>
2026-02-24 12:10:52 -0800

    Update `allowedUnfreePredicate`

--- modules/base/unfree-packages.nix
+++ modules/base/unfree-packages.nix
@@ -7,9 +7,7 @@
 }: {
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
-      "chatgpt"
       "claude-code"
-      "claude-desktop"
       "daisydisk"
       "fastscripts"
       "mochi"
