d79526cb65eb093dd562f47ff2250495c5459c22
✿ corey (they/them) <c@rey.foo>
2026-02-25 15:03:02 -0800

    Rev nixpkgs-stable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
     # General
     systems = {
