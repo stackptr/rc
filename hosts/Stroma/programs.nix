780001201edd74e6bf8c97808dfaba51d029c21a
✿ corey (they/them) <c@rey.foo>
2026-04-08 16:56:26 -0700

    add Things

--- hosts/Stroma/programs.nix
+++ hosts/Stroma/programs.nix
@@ -11,6 +11,10 @@
 
   homebrew = {
     enable = true;
+    # N.B.: Removed entries in `masApps` require manual uninstallation
+    masApps = {
+      "Things" = 904280696;
+    };
   };
 
   programs.popclip = {
