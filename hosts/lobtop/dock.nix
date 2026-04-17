9ced3669e0ee6af15dc9041a35013f825bedf1ff
✿ corey (they/them) <c@rey.foo>
2026-04-17 12:35:55 -0700

    feat: update dock items

--- hosts/lobtop/dock.nix
+++ hosts/lobtop/dock.nix
@@ -4,9 +4,16 @@ _: {
     largesize = 86;
     persistent-apps = [
       "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app"
+      "/System/Applications/Mail.app"
+      "/Applications/Zed.app"
+      "/Applications/rootshell.app"
+      "/Applications/Reeder.app"
       "/System/Applications/Calendar.app"
       "/System/Applications/Reminders.app"
       "/System/Applications/Notes.app"
+      "/Applications/Things3.app"
+      "/Applications/Craft.app"
+      "/System/Applications/Messages.app"
       "/Applications/Slack.app"
     ];
   };
